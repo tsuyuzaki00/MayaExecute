806f73646987c904125554fc4cab525826ca5cff
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2021-01-06 19:05:54 +0900

    随時mainEditから更新中

---
+++ getNameSplit_autoRename.py
@@ -0,0 +1,18 @@
+import pymel.core as pm
+import sys
+import os
+sys.path.append(os.path.abspath(".."))
+from MayaLibrary import getNameSplit as gns
+
+def main():
+    sels = pm.selected()
+    for sel in sels:
+        scene = gns.scene(sel)
+        node = gns.node(sel)
+        obj = gns.obj(sel)
+        pos = gns.pos(sel)
+        num = gns.num(sel)
+        lists = [pos,obj,node,scene,num]
+        names = [l for l in lists if l != '']
+        autoRename = '_'.join(names)
+        pm.rename(sel, autoRename)
